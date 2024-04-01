f1409207d5720aa3b6a656b77b33aca6d08cdc6c
fathimaththasneem <fathimaththasnee@gmail.com>
2024-04-01 12:57:08 +0530

    add initail code-tailwind config

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center text-7xl text-gray-600 underline">
+      ecom frondend
+     
+    </div>
+  );
+}
+
+export default App;
